bef9ba24f95822f55f163a0e1d053aab784682f0
Muhammad Faris Hakim <81744011+AgentHitmanFaris@users.noreply.github.com>
2023-06-26 15:43:58 +0800

    Update Main.py

--- Code/Main.py
+++ Code/Main.py
@@ -13,14 +13,27 @@ xml_content = response.text
 # Create a BeautifulSoup object to parse the XML content
 soup = BeautifulSoup(xml_content, "xml")
 txt = soup.find_all("name")
-x = [element.text.split("#", 1) for element in txt]
+x = [element.text.split(" ", 1) for element in txt]
 
-# Print each data on a new line
+# Initialize an empty list to store module codes
+module_codes = []
+
+# Prompt the user to enter the module code
+module_code = input("Enter the module code (or leave blank to exit): ").upper()
+
+while module_code:
+    module_codes.append(module_code)
+    module_code = input("Enter another module code (or leave blank to exit): ").upper()
+
+# Print the two sections and scrape the link attribute value for each module code
 for data in x:
-    if len(data) == 2:
-        section1, section2 = data
-        print("Section 1:")
-        print(section1.strip()) # Remove leading/trailing whitespaces
+  section1, section2 = data
+  section1_parts = section1.strip()
+  section2_parts = section2.split(", ")
 
-# Find the specific module
-module_code = "AA10203"
+  if section1_parts in module_codes:
+    print("Section 1:")
+    print(section1_parts)
+    print("Section 2:")
+    print(section2_parts[0].strip())
+    print()
